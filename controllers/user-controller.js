6d2be35443254c3b931b48d3fbb8f5ae6fb95c42
kennykenny19970314@gmail.com <kennykenny19970314@gmail.com>
2024-03-26 18:56:21 +0800

    user signup

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,38 @@
+const db = require('../models')
+const { User } = db
+const bcrypt = require('bcryptjs')
+
+/**************************************************************** */
 const userController = {
+  signUpPage: (req, res) => {
+    return res.render('signup')
+  },
+  signUp: (req, res, next) => {
+    const newuser = req.body
+    console.log(newuser)
+    if (!newuser.email) throw new Error('email必填')
+    if (newuser.password !== newuser.passwordCheck) throw new Error('password請重新確認')
+    return User.findOne({
+      where: { email: newuser.email }
+    })
+      .then((u) => {
+        if (u) throw new Error('email已經被註冊')
+        bcrypt.hash(newuser.password, 10).then((hash) => {
+          return User.create({
+            name: newuser.name,
+            email: newuser.email,
+            password: hash
+          })
+        })
+      })
+      .then(() => {
+        res.redirect('/login')
+      })
+      .catch((err) => next(err))
+  },
   login: (req, res) => {
     return res.render('login')
-  },
-  signup: (req, res) => {
-    return res.render('signup')
   }
 }
+/************************************************************* */
 module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/user-controller')
-
+/****************************************************************** */
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/login', userController.login)
-router.get('/signup', userController.signup)
 
 router.use('/', (req, res) => res.redirect('/login'))
 module.exports = router
